34c69c035356412eeb619cfb0086a81de44787d7
John Cai <johncai117@gmail.com>
2021-02-15 12:18:51 +0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-917d87db45abc2af64f6.js"
+    "url": "webpack-runtime-0f97c6cbe476338262aa.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f0570c5cb8b5ca9cf83b8b48d50cf16d"
+    "revision": "b8b3be6f4f8040d6fa2b42dd4b0f3b78"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
