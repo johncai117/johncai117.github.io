b2d42df79349c64c803cab331c245a2bb69e01aa
John Cai <johncai117@gmail.com>
2022-08-14 16:24:56 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8913fdad191cd512eb98.js"
+    "url": "webpack-runtime-bf43fcda9263b023a931.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "f3500521abdaab9c14678b3b0a260c24"
+    "revision": "b72ead4627a2092b6bf1ccccea9efb18"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
